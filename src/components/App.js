eac7aee5df6c6fb8a7c4a949eb1848efbf44ea7d
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2023-11-12 00:48:21 +0530

    selecting & showing correct answer

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ const initialState = {
   // "loading"."error",ready,active,finished
   status: "loading",
   index: "0",
+  answer: null,
 };
 
 function reducer(state, action) {
@@ -33,12 +34,17 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+      };
     default:
       throw new Error("Action is unknown");
   }
 }
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -59,7 +65,13 @@ function App() {
         {status === "ready" && (
           <StartScreen numofQuestions={numofQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,20 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer != null;
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          disabled={answer !== null}
+        >
           {option}
         </button>
       ))}
